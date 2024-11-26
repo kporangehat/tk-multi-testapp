bb883115ea8fb0a11d5f8a440933d219e0686b82
KP <kporangehat@gmail.com>
2024-11-26 16:53:01 -0600

    app showing sg proxy model filter issues - filtering overwrites itself because thumbnail data calls the filter on each row a second time after the data has been set.

--- python/app/dialog.py
+++ python/app/dialog.py
@@ -9,14 +9,15 @@
 # not expressly granted therein are reserved by Shotgun Software Inc.
 
 import sgtk
-import os
-import sys
-import threading
 
-# by importing QT from sgtk rather than directly, we ensure that
-# the code will be compatible with both PySide and PyQt.
 from sgtk.platform.qt import QtCore, QtGui
 from .ui.dialog import Ui_Dialog
+
+shotgun_model = sgtk.platform.import_framework(
+    "tk-framework-shotgunutils", "shotgun_model"
+)
+SimpleShotgunModel = shotgun_model.SimpleShotgunModel
+
 
 # standard toolkit logger
 logger = sgtk.platform.get_logger(__name__)
@@ -32,7 +33,59 @@ def show_dialog(app_instance):
 
     # we pass the dialog class to this method and leave the actual construction
     # to be carried out by toolkit.
-    app_instance.engine.show_dialog("Starter Template App...", app_instance, AppDialog)
+    app_instance.engine.show_dialog("Test App", app_instance, AppDialog)
+
+
+class AlphaProxyModel(QtGui.QSortFilterProxyModel):
+    """
+    Proxy model to sort ShotgunModel items alphanumerically
+    """
+    def lessThan(self, left, right):
+        """
+        Overrides the base class method to sort the model by content.
+
+        Args:
+            left (QModelIndex): The left index.
+            right (QModelIndex): The right index.
+
+        Returns:
+            bool: True if the left index is less than the right index, False otherwise.
+        """
+        source_model = self.sourceModel()
+        lvalue = source_model.itemFromIndex(left).data(QtCore.Qt.DisplayRole)
+        rvalue = source_model.itemFromIndex(right).data(QtCore.Qt.DisplayRole)
+        return lvalue < rvalue
+
+
+class UniqueNameProxyModel(AlphaProxyModel):
+    """
+    Proxy model to filter out duplicate content
+    """
+    def filterAcceptsRow(self, source_row, source_parent):
+        """
+        Overrides the base class method to filter out duplicate content.
+
+        Args:
+            source_row (int): The row number in the source model.
+            source_parent (QModelIndex): The parent index in the source model.
+        """
+        try:
+            source_model = self.sourceModel()
+            content_index = source_model.index(source_row, 0, source_parent)
+            content = source_model.data(content_index, QtCore.Qt.DisplayRole)
+
+            # Check if the content is already in the view
+            for row in range(self.rowCount()):
+                index = self.index(row, 0)
+                existing_content = self.data(index, QtCore.Qt.DisplayRole)
+                if content == existing_content:
+                    return False
+            return super(UniqueNameProxyModel, self).filterAcceptsRow(source_row, source_parent)
+        except Exception as e:
+            logger.error(
+                f"An error occurred while filtering content: {e}", exc_info=True
+            )
+            return False
 
 
 class AppDialog(QtGui.QWidget):
@@ -44,24 +97,115 @@ class AppDialog(QtGui.QWidget):
         """
         Constructor
         """
-        # first, call the base class and let it do its thing.
-        QtGui.QWidget.__init__(self)
-
-        # now load in the UI that was created in the UI designer
+        super(AppDialog, self).__init__()
         self.ui = Ui_Dialog()
         self.ui.setupUi(self)
 
-        # most of the useful accessors are available through the Application class instance
-        # it is often handy to keep a reference to this. You can get it via the following method:
+        logger.info("Launching Test App...")
         self._app = sgtk.platform.current_bundle()
 
-        # logging happens via a standard toolkit logger
-        logger.info("Launching Starter Application...")
+        self.ui.asset_tree_view.setIndentation(16)
+        self.ui.asset_tree_view.setUniformRowHeights(True)
+        self.ui.asset_tree_view.setSortingEnabled(True)
+        self.ui.asset_tree_view.sortByColumn(0, QtCore.Qt.AscendingOrder)
 
-        # via the self._app handle we can for example access:
-        # - The engine, via self._app.engine
-        # - A Shotgun API instance, via self._app.shotgun
-        # - An Sgtk API instance, via self._app.sgtk
+        # ------------------------------
+        # Entity model
+        # ------------------------------
+        self._entity_model = shotgun_model.ShotgunEntityModel(
+            "Asset",  # entity type
+            [["project", "is", self._app.context.project]],  # filters
+            ["project.Project.name", "sg_asset_type", "code"],  # hierarchy
+            ["description", "id", "project", "sg_asset_type"],  # fields
+            self,
+        )
+        self._entity_model.async_refresh()
+        self._entity_proxy_model = QtGui.QSortFilterProxyModel(self)
+        self._entity_proxy_model.setDynamicSortFilter(True)
+        self._entity_proxy_model.setSourceModel(self._entity_model)
+        self.ui.asset_tree_view.setModel(self._entity_proxy_model)
+        # selections
+        self.ui.asset_tree_view.selectionModel().selectionChanged.connect(
+            self.handle_asset_selection
+        )
 
-        # lastly, set up our very basic UI
-        self.ui.context.setText("Current Context: %s" % self._app.context)
+        # ------------------------------
+        # Tasks model
+        # ------------------------------
+        self._entity_tasks_model = shotgun_model.ShotgunModel(self)
+        self._entity_tasks_model.data_refreshed.connect(self.on_task_data_refreshed)
+        self._entity_tasks_proxy_model = UniqueNameProxyModel(self)
+        self._entity_tasks_proxy_model.setDynamicSortFilter(True)
+        self._entity_tasks_proxy_model.setSourceModel(self._entity_tasks_model)
+        self.ui.task_list_view.setModel(self._entity_tasks_proxy_model)
+
+        #
+        # Buttons and checkboxes
+        #
+        self.ui.refresh_button.clicked.connect(self.load_task_data)
+        self.ui.clear_cache_button.clicked.connect(self._entity_tasks_model.hard_refresh)
+        self.ui.dynamic_sort_checkbox.setChecked(self._entity_tasks_proxy_model.dynamicSortFilter())
+        self.ui.dynamic_sort_checkbox.stateChanged.connect(
+            self.on_dynamic_sort_changed
+        )
+
+    def destroy(self):
+        """
+        Destroy the model as required by the API.
+        """
+        try:
+            self._entity_model.destroy()
+            self._entity_tasks_model.destroy()
+        except Exception as e:
+            # log exception
+            pass
+
+    def handle_asset_selection(self, selected, deselected):
+        """
+        Handle when an asset is selected in the tree view.
+
+        Args:
+            selected (QItemSelection): The selected items.
+            deselected (QItemSelection): The deselected items.
+        """
+        try:
+            indexes = selected.indexes()
+            if not indexes:
+                return
+            # get the selected entity
+            index = indexes[0]
+            entity = self._entity_proxy_model.data(
+                index, shotgun_model.ShotgunEntityModel.SG_DATA_ROLE
+            )
+            # update the active entity
+            self.active_entity = entity
+            self.load_task_data()
+        except Exception as e:
+            logger.error(f"*** Error selecting source task: {e}")
+
+    def load_task_data(self):
+        # self._entity_tasks_proxy_model.invalidate()
+        self._entity_tasks_model._load_data(
+            "Task",
+            fields=["content", "sg_task_token", "step.Step.short_name"],
+            filters=[
+                ["entity", "is", self.active_entity],
+            ],
+            hierarchy=["content"],
+            # We could set the order here, but it's not the "correct" way to sort the model.
+            # and we may have more complex sorting requirements in the future.
+            # order=[{"field_name": "content", "direction": "asc"}],
+        )
+        self._entity_tasks_model._refresh_data()
+
+    def on_task_data_refreshed(self):
+        self._entity_tasks_proxy_model.sort(0, QtCore.Qt.AscendingOrder)
+
+    def clear_cache(self):
+        self._entity_tasks_model.clear()
+        self._entity_tasks_model._refresh_data()
+
+    def on_dynamic_sort_changed(self, state):
+        logger.debug(f"*** _entity_tasks_proxy_model setDynamicSortFilter: {state}")
+        self._entity_tasks_proxy_model.setDynamicSortFilter(state)
+        self._entity_tasks_model._refresh_data()
--- python/app/ui/dialog.py
+++ python/app/ui/dialog.py
@@ -23,25 +23,66 @@ class Ui_Dialog(object):
     def setupUi(self, Dialog):
         if not Dialog.objectName():
             Dialog.setObjectName(u"Dialog")
-        Dialog.resize(431, 392)
-        self.horizontalLayout = QHBoxLayout(Dialog)
-        self.horizontalLayout.setObjectName(u"horizontalLayout")
-        self.logo_example = QLabel(Dialog)
-        self.logo_example.setObjectName(u"logo_example")
-        self.logo_example.setPixmap(QPixmap(u":/res/sg_logo.png"))
+        Dialog.resize(700, 500)
+        self.verticalLayout_2 = QVBoxLayout(Dialog)
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.buttonBox = QDialogButtonBox(Dialog)
+        self.buttonBox.setObjectName(u"buttonBox")
+        self.buttonBox.setStandardButtons(QDialogButtonBox.StandardButton.Cancel|QDialogButtonBox.StandardButton.Ok)
 
-        self.horizontalLayout.addWidget(self.logo_example)
+        self.verticalLayout_2.addWidget(self.buttonBox)
 
-        self.context = QLabel(Dialog)
-        self.context.setObjectName(u"context")
-        sizePolicy = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.context.sizePolicy().hasHeightForWidth())
-        self.context.setSizePolicy(sizePolicy)
-        self.context.setAlignment(Qt.AlignLeading|Qt.AlignLeft|Qt.AlignVCenter)
+        self.header_label = QLabel(Dialog)
+        self.header_label.setObjectName(u"header_label")
 
-        self.horizontalLayout.addWidget(self.context)
+        self.verticalLayout_2.addWidget(self.header_label)
+
+        self.horizontalLayout_2 = QHBoxLayout()
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.verticalLayout = QVBoxLayout()
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.task_list_header = QLabel(Dialog)
+        self.task_list_header.setObjectName(u"task_list_header")
+
+        self.verticalLayout.addWidget(self.task_list_header)
+
+        self.asset_tree_view = QTreeView(Dialog)
+        self.asset_tree_view.setObjectName(u"asset_tree_view")
+
+        self.verticalLayout.addWidget(self.asset_tree_view)
+
+        self.clear_cache_button = QPushButton(Dialog)
+        self.clear_cache_button.setObjectName(u"clear_cache_button")
+
+        self.verticalLayout.addWidget(self.clear_cache_button)
+
+        self.horizontalLayout_2.addLayout(self.verticalLayout)
+
+        self.verticalLayout_3 = QVBoxLayout()
+        self.verticalLayout_3.setObjectName(u"verticalLayout_3")
+        self.asset_tree_header = QLabel(Dialog)
+        self.asset_tree_header.setObjectName(u"asset_tree_header")
+
+        self.verticalLayout_3.addWidget(self.asset_tree_header)
+
+        self.task_list_view = QListView(Dialog)
+        self.task_list_view.setObjectName(u"task_list_view")
+
+        self.verticalLayout_3.addWidget(self.task_list_view)
+
+        self.dynamic_sort_checkbox = QCheckBox(Dialog)
+        self.dynamic_sort_checkbox.setObjectName(u"dynamic_sort_checkbox")
+
+        self.verticalLayout_3.addWidget(self.dynamic_sort_checkbox)
+
+        self.refresh_button = QPushButton(Dialog)
+        self.refresh_button.setObjectName(u"refresh_button")
+
+        self.verticalLayout_3.addWidget(self.refresh_button)
+
+        self.horizontalLayout_2.addLayout(self.verticalLayout_3)
+
+        self.verticalLayout_2.addLayout(self.horizontalLayout_2)
 
         self.retranslateUi(Dialog)
 
@@ -50,6 +91,10 @@ class Ui_Dialog(object):
 
     def retranslateUi(self, Dialog):
         Dialog.setWindowTitle(QCoreApplication.translate("Dialog", u"The Current Sgtk Environment", None))
-        self.logo_example.setText("")
-        self.context.setText(QCoreApplication.translate("Dialog", u"Your Current Context: ", None))
+        self.header_label.setText(QCoreApplication.translate("Dialog", u"Select any Asset in the tree and see the corresponding Tasks", None))
+        self.task_list_header.setText(QCoreApplication.translate("Dialog", u"Asset Tree", None))
+        self.clear_cache_button.setText(QCoreApplication.translate("Dialog", u"Clear Cache", None))
+        self.asset_tree_header.setText(QCoreApplication.translate("Dialog", u"Associated Tasks", None))
+        self.dynamic_sort_checkbox.setText(QCoreApplication.translate("Dialog", u"Click to toggle setDynamicSortFilter", None))
+        self.refresh_button.setText(QCoreApplication.translate("Dialog", u"Reload Tasks", None))
     # retranslateUi
